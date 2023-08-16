8f3807282bea262290291f1c865e7554e3d70058
countrsignal <dpenaher@protonmail.com>
2023-08-16 13:09:12 -0400

    fix import stall

--- pjm/model/__init__.py
+++ pjm/model/__init__.py
@@ -2,6 +2,7 @@ from .gvp_gnn import GVPGNN
 from .decoder import BaselineDecoder, CXDecoderBlock, StandardDecoderBlock, MultiModalDecoder
 from .attention import Transformer, CrossAttention, AttnLayerNorm, Residual, SwiGLU, get_attn_mask
 from .experimental import standard_structure_module, CoCa
+from .embedder import from_pretrained, Embedder
 from .baseline import BaselineModel
 from .masking import (
     highlight_mask,
